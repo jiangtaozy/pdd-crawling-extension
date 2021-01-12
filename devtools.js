5341b9005402bb200cdb7a406b968dd590f3c8e6
jemo <1321332562@qq.com>
2021-01-12 16:26:38 +0800

    sync pdd item

--- devtools.js
+++ devtools.js
@@ -6,7 +6,8 @@ chrome.devtools.network.onRequestFinished.addListener(
     } = request.request;
     if(url === 'https://yingxiao.pinduoduo.com/mms-gateway/venus/api/unit/listPage' ||
       url === 'https://yingxiao.pinduoduo.com/venus/api/subway/keyword/listKeywordPage' ||
-      url === 'https://yingxiao.pinduoduo.com/apollo/api/report/queryHourlyReport') {
+      url === 'https://yingxiao.pinduoduo.com/apollo/api/report/queryHourlyReport' ||
+      url === 'https://mms.pinduoduo.com/vodka/v2/mms/query/display/mall/goodsList') {
       request.getContent(async function(content, encoding) {
         try {
           await axios.post('http://localhost:7000/saveNetworkData', {
