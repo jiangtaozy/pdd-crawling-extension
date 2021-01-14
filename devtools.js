05569c9eede3897cfae74dba9811c04f09fdce5e
jemo <1321332562@qq.com>
2021-01-14 17:23:29 +0800

    sync pdd order

--- devtools.js
+++ devtools.js
@@ -7,6 +7,7 @@ chrome.devtools.network.onRequestFinished.addListener(
     if(url === 'https://yingxiao.pinduoduo.com/mms-gateway/venus/api/unit/listPage' ||
       url === 'https://yingxiao.pinduoduo.com/venus/api/subway/keyword/listKeywordPage' ||
       url === 'https://yingxiao.pinduoduo.com/apollo/api/report/queryHourlyReport' ||
+      url === 'https://mms.pinduoduo.com/mangkhut/mms/recentOrderList' ||
       url === 'https://mms.pinduoduo.com/vodka/v2/mms/query/display/mall/goodsList') {
       request.getContent(async function(content, encoding) {
         try {
