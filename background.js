8fdaec66abbfc975b4a2b424209dc6457ee574f6
jemo <1321332562@qq.com>
2025-04-06 09:52:10 +0800

    version 3

--- background.js
+++ background.js
@@ -1,24 +1,24 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'mms.pinduoduo.com'},
-        }),
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'yingxiao.pinduoduo.com'},
-        }),
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'www.hznzcn.com'},
-        }),
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
-
-chrome.extension.onMessage.addListener(function (message, sender) {
-  if(message === "goback") {
-    chrome.tabs.goBack();
-  }
-});
+//chrome.runtime.onInstalled.addListener(function() {
+//  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//    chrome.declarativeContent.onPageChanged.addRules([{
+//      conditions: [
+//        new chrome.declarativeContent.PageStateMatcher({
+//          pageUrl: {hostEquals: 'mms.pinduoduo.com'},
+//        }),
+//        new chrome.declarativeContent.PageStateMatcher({
+//          pageUrl: {hostEquals: 'yingxiao.pinduoduo.com'},
+//        }),
+//        new chrome.declarativeContent.PageStateMatcher({
+//          pageUrl: {hostEquals: 'www.hznzcn.com'},
+//        }),
+//      ],
+//      actions: [new chrome.declarativeContent.ShowPageAction()]
+//    }]);
+//  });
+//});
+//
+//chrome.extension.onMessage.addListener(function (message, sender) {
+//  if(message === "goback") {
+//    chrome.tabs.goBack();
+//  }
+//});
